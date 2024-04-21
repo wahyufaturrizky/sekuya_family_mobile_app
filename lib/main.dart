c93008e7471884830bd4c054661cab720d48b5da
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2024-04-21 23:54:17 +0700

    FEAT: set firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,16 @@
 
 import 'components/app/app_component.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+
   runApp(const AppComponent());
 }
--- lib/screens/login/login_component.dart
+++ lib/screens/login/login_component.dart
@@ -7,10 +7,27 @@
  * See LICENSE for distribution and usage details.
  */
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:sekuya_family_mobile_app/components/components.dart';
 import 'package:sekuya_family_mobile_app/config/application.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+/// The scopes required by this application.
+// #docregion Initialize
+const List<String> scopes = <String>[
+  'email',
+  'https://www.googleapis.com/auth/contacts.readonly',
+];
+
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  // Optional clientId
+  clientId:
+      '433294916757-ebvrl9qvhgvn3vqo3j2k9elirj7t1k7r.apps.googleusercontent.com',
+  scopes: scopes,
+);
+// #enddocregion Initialize
 
 class LoginScreenApp extends StatelessWidget {
   const LoginScreenApp({super.key});
@@ -29,6 +46,55 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  GoogleSignInAccount? _currentUser;
+  bool _isAuthorized = false; // has granted permissions?
+  String _contactText = '';
+  ValueNotifier userCredential = ValueNotifier('');
+
+  @override
+  void initState() {
+    print('--- initState ---');
+
+    print('userCredential = $userCredential["value"]');
+    super.initState();
+
+    _googleSignIn.onCurrentUserChanged
+        .listen((GoogleSignInAccount? account) async {
+      bool isAuthorized = account != null;
+
+      print('DEBUG = $isAuthorized');
+
+      setState(() {
+        _currentUser = account;
+        _isAuthorized = isAuthorized;
+      });
+    });
+  }
+
+  void handleLoginGoogle() async {
+    // Application.router.navigateTo(context, "/privatescreens",
+    //     transition: TransitionType.native);
+  }
+
+  Future<dynamic> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      return await FirebaseAuth.instance.signInWithCredential(credential);
+    } on Exception catch (e) {
+      // TODO
+      print('exception->$e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -71,9 +137,7 @@ class _LoginScreenState extends State<LoginScreen> {
                             child: CustomButton(
                                 buttonText: 'Continue with Gmail',
                                 onPressed: () {
-                                  Application.router.navigateTo(
-                                      context, "/privatescreens",
-                                      transition: TransitionType.native);
+                                  signInWithGoogle();
                                 },
                                 buttonIcon: "ic_google.png",
                                 isOutlined: true,
