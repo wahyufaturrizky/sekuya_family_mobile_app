4ae1a392ad97e53270b14e00fc6e743d3be025c7
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2024-04-30 21:12:20 +0700

    FEAT: Install our SDK to get in-app feedback from your testers

--- android/app/src/main/kotlin/com/sekuya/familyapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/sekuya/familyapp/MainActivity.kt
@@ -2,4 +2,14 @@ package com.sekuya.familyapp
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Firebase.appDistribution.showFeedbackNotification(
+            // Text providing notice to your testers about collection and
+            // processing of their feedback data
+            R.string.additionalFormText,
+            // The level of interruption for the notification
+            InterruptionLevel.HIGH)
+    }
+}
